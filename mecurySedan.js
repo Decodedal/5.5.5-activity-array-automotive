e30ee5296ab5624f3b444859af367b9016d04389
Decodedal <Palumbodallas@gmail.com>
2022-06-22 16:06:34 -0400

    finished

--- mecurySedan.js
+++ mecurySedan.js
@@ -22,6 +22,7 @@ class Car extends VehicleModule{
     start(){
         this.fule > 0 ? console.log("The car starts") : console.log("The car wont start")
     }
+    //does the car neede to be serviced
     checkService(){
         this.mileage >= 30000 ? this.scheduleService = true : this.scheduleService = false
         console.log(this.scheduleService)
@@ -37,6 +38,32 @@ myCar.checkService()
 
 console.log(myCar)
 
+// new truck class that extends Vehicle
+class Truck extends VehicleModule{
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 7;
+        this.passenger = 0;
+        this.numberOfWheels = 4; 
+        this.maximumSpeed = 100;
+        this.fule = 10;
+        this.scheduleService = false;
+        this.allTerrain = true
+    }
+    //Is this an all wheel drive 
+    driveType(){
+        this.allTerrain == true ? console.log("This vehicle has all wheel drive") : console.log("This vehicle has rear wheel drive")
+    }
+    //should the driver turn on overdrive
+    overDrive(speed){
+        speed > this.maximumSpeed ? console.log("Turn on the overdrive") : ("No overdrive needed")
+    }
+}
+let F150 = new Truck ('Ford', 'F150', 2020, 'Red', 50000)
+
+F150.driveType()
+F150.overDrive(130)
+F150.typeOfVehicle()
 
 
 
